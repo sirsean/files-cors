7d0b18575cbfd988bdfafaed98a44c49101be215
sirsean <sirsean@gmail.com>
2022-05-28 13:51:28 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,20 @@
+export default {
+    async fetch(request) {
+        const url = new URL(request.url);
+        url.protocol = 'https';
+        url.hostname = 'files-3d9.pages.dev';
+        return fetch(url.toString(), request)
+            .then(response => {
+                const headers = response.headers;
+                headers.set('Access-Control-Allow-Origin', '*');
+                headers.set('Access-Control-Allow-Methods': 'GET,HEAD');
+                return new Response(response.body,
+                    {
+                        status: response.status,
+                        statusText: response.statusText,
+                        headers: headers,
+                    });
+            );
+            });
+    },
+};
